943b12aabd7476ad1f14a2703d54ae1101be11c4
Daniel Zambrano <dzambrano863@gmail.com>
2023-07-25 22:53:00 -0500

    first commit

---
+++ SparkyMSTest/CalculatorMSTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sparky;
+
+namespace SparkyMSTest
+{
+    [TestClass]
+    public class CalculatorMSTest
+    {
+        [TestMethod]
+        public void AddNumbers_InputTwoInt_GetCorrectAddition()
+        {
+            //Arrange
+            Calculator calc = new();
+            //Act
+            int result = calc.AddNumbers(10, 20);
+
+            //Assert
+            Assert.AreEqual(30, result);
+
+        }
+    }
+
+
+}
---
+++ SparkyNUnitTest/CalculatorNUnitTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace Sparky
+{
+    [TestFixture]
+    public class CalculatorNUnitTests
+    {
+        [Test]
+        public void AddNumbers_InputTwoInt_GetCorrectAddition()
+        {
+            //Arrange
+            Calculator calc = new();
+            //Act
+            int result = calc.AddNumbers(10, 20);
+
+            //Assert
+            Assert.AreEqual(30, result);
+
+        }
+    }
+}
